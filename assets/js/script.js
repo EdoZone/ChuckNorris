1a82364f3c2dd7e764bbd29cf7bcd490ac25c1bf
Eduardo Nunez H <edo_zone@hotmail.com>
2024-10-15 23:30:10 -0300

    Agregado "Cargando datos..."

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,19 @@ let frase = document.getElementById("frase");
 
 const obtenerDatos = async () => {
   try {
+    frase.innerHTML = "Cargando datos..."
     const response = await fetch("https://api.chucknorris.io/jokes/random");
     const datos = await response.json();
     frase.innerHTML = datos.value
+
+    
   } catch (error) {
     alert(
       "Se ha interrumpido la conexxion con el servidor y no se puede obtener la frase"
     );
   }
 };
+
+// const url = `https://api.mymemory.translated.net/get?q=${datos.value}&langpair=en|es`;
 
 obtenerDatos();
